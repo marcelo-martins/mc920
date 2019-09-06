c0cf25e3aed34dbe0ad763ddf253a7cb15471816
marcelo-martins <marcelobmartins219@hotmail.com>
2019-09-06 01:50:37 -0300

    function generic

--- imagens/proj1/proj1.py
+++ imagens/proj1/proj1.py
@@ -5,6 +5,30 @@ import matplotlib.pyplot as plt
 import os
 import glob
 
+floyd = np.array([[0, 0, 7/16],
+           [3/16, 5/16, 1/16]]).reshape(2,3)
+
+stevenson = np.array([[0 , 0 , 0 , 0 , 0 , 32/200 , 0],
+            [12/200, 0 , 26/200 , 0 , 30/200 , 0 , 16/200],
+            [0 , 12/200 , 0 , 26/200 , 0 , 12/200 , 0],
+            [5/200 , 0 , 12/200 , 0 , 12/200 , 0 , 5/200]]).reshape(4,7)
+
+burkes = np.array([[0 , 0 , 0 , 8/32 , 4/32],
+         [2/32 , 4/32 , 8/32 , 4/32 , 2/32]]).reshape(2,5)
+
+sierra = np.array([[0 , 0 , 0 , 5/32 , 3/32],
+            [2/32 , 4/32 , 5/32 , 4/32 , 2/32],
+            [0 , 2/32 , 3/32 , 2/32 , 0]]).reshape(3, 5)
+
+stucki = np.array([[0 , 0 , 0, 8/32 , 4/42],
+            [2/42 , 4/42 , 8/42 , 4/42 , 2/32],
+            [1/42 , 2/42 , 4/42 , 2/42 , 1/42]]).reshape(3,5)
+
+jarvis = np.array([[0 , 0 , 0, 7/48 , 5/48],
+            [3/48 , 5/48 , 7/48 , 5/48 , 3/48],
+            [1/48 , 3/48 , 5/48 , 3/48 , 1/48]]).reshape(3,5)
+
+
 #Getting all files
 def getImages(color, image_dir):
     data_path = os.path.join(image_dir, '*g')
@@ -13,7 +37,7 @@ def getImages(color, image_dir):
     names = []
     if (color=="colorful"):
         for fl in files:
-            img = cv2.cvtColor(cv2.imread(fl), cv2.COLOR_BGR2RGB)
+            img = cv2.imread(fl, cv2.IMREAD_COLOR)
             data.append(img)
             names.append(os.path.splitext(os.path.basename(fl))[0])
     elif(color == "gray"):
@@ -25,7 +49,8 @@ def getImages(color, image_dir):
         print("please specify color type")
     return data, names
 
-def goThroughGray(matrix, bordersize):
+def goThroughGray(matrix, bordersize, filtro):
+    startOfFilter = math.floor(filtro.shape[1]/2)
 
     (row,column) = matrix.shape
     print(row, column)
@@ -34,55 +59,67 @@ def goThroughGray(matrix, bordersize):
     row-= bordersize
     column-=bordersize
 
+    interval = [column, bordersize]
 
 
     # for i in range(bordersize, row):
     #     for j in range(bordersize, column):
     #         print(matrix[i,j])
 
+    one = 1
     #origem fica em cima
     for i in range(bordersize, row):#i row, j column
     #for i in range(0, row):#i row, j column
-        if(i%2!=0):
+        #if(i%2!=0):
             # for j in reversed(range(bordersize, column)):
-            for j in range(column-1, bordersize-1, -1):
-            #for j in range(column-1, -1, -1):
-                #print(j)
-                if(matrix[i,j] < 128):
-                    matrixAux[i,j] = 0
-                else:
-                    matrixAux[i,j] = 255
-                err = float(matrix[i,j] - matrixAux[i,j])
-                #print(matrix[i,j], matrixAux[i,j], err)
-                #if(i>0 and j-1<column-1):
-                matrix[i,j-1] += (7/16)*err
-                #if(i+1<row-1):
-                    #if(j+1<column-1):
-                matrix[i+1,j+1] += (3/16)*err
-                    #if(j<column-1):
-                matrix[i+1, j] += (5/16)*err
-                    #if(j-1<column-1):
-                matrix[i+1,j-1] += (1/16)*err
+        temp = interval[0]
+        interval[0] = interval[1]
+        interval[1] = temp
+        one = -one
+        #print(i, interval[1], interval[0], one)
+        for j in range(interval[1], interval[0], one):
+        #for j in range(column-1, -1, -1):
+            #print(j)
+            if(matrix[i,j] < 128):
+                matrixAux[i,j] = 0
+            else:
+                matrixAux[i,j] = 255
+            err = float(matrix[i,j] - matrixAux[i,j])
+            #print(err)
+            #print(matrix[i,j], matrixAux[i,j], err)
+            #if(i>0 and j-1<column-1):
+            # matrix[i,j+one] += (7/16)*err
+            # #if(i+1<row-1):
+            #     #if(j+1<column-1):
+            # matrix[i+1,j-one] += (3/16)*err
+            #     #if(j<column-1):
+            # matrix[i+1, j] += (5/16)*err
+            #     #if(j-1<column-1):
+            # matrix[i+1,j+one] += (1/16)*err
+
+            for g in range(0, filtro.shape[0]):
+                for k in range(0, filtro.shape[1]):
+                    matrix[i+g, j+((k-startOfFilter)*one)] += filtro[g,k] * err
             
-        else:
-            for j in range(bordersize, column):
-            #for j in range(0, column):
-                #print(j)
-                if(matrix[i,j] < 128):
-                    matrixAux[i,j] = 0
-                else:
-                    matrixAux[i,j] = 255
-                err = float(matrix[i,j] - matrixAux[i,j])
-                #print(err)
-                #if(i>0 and j+1<column-1):
-                matrix[i, j+1] += (7/16)*err
-                #if(i+1<row-1):
-                    #if(j-1<column-1):
-                matrix[i+1,j-1] += (3/16)*err
-                    #if(j<column-1):
-                matrix[i+1, j] += (5/16)*err
-                    #if(j+1<column-1):
-                matrix[i+1,j+1] += (1/16)*err
+        # else:
+        #     for j in range(bordersize, column):
+        #     #for j in range(0, column):
+        #         #print(j)
+        #         if(matrix[i,j] < 128):
+        #             matrixAux[i,j] = 0
+        #         else:
+        #             matrixAux[i,j] = 255
+        #         err = float(matrix[i,j] - matrixAux[i,j])
+        #         #print(err)
+        #         #if(i>0 and j+1<column-1):
+        #         matrix[i, j+1] += (7/16)*err
+        #         #if(i+1<row-1):
+        #             #if(j-1<column-1):
+        #         matrix[i+1,j-1] += (3/16)*err
+        #             #if(j<column-1):
+        #         matrix[i+1, j] += (5/16)*err
+        #             #if(j+1<column-1):
+        #         matrix[i+1,j+1] += (1/16)*err
 
 
     # plt.axis("off")
@@ -101,11 +138,12 @@ def goThroughGray(matrix, bordersize):
     #     for j in range(bordersize, column):
     #         print(matrixAux[i,j])
 
+
     return matrixAux
 
 
 
-def testColor(title, matrixColor):
+def testColor(title, matrixColor, filtro):
    
     #matrixColor = cv2.cvtColor(cv2.imread("train.png"), cv2.COLOR_BGR2RGB)
    
@@ -124,14 +162,14 @@ def testColor(title, matrixColor):
     # plt.close("all")
 
     #chama a funcao pras 3
-    matrixR = goThroughGray(matrixR, bordersize)
-    matrixG = goThroughGray(matrixG, bordersize)
-    matrixB = goThroughGray(matrixB, bordersize)
+    matrixR = goThroughGray(matrixR, bordersize, filtro)
+    matrixG = goThroughGray(matrixG, bordersize, filtro)
+    matrixB = goThroughGray(matrixB, bordersize, filtro)
 
 
    
     
-    matrixColor = cv2.merge((matrixR, matrixG, matrixB))
+    matrixColor = cv2.merge((matrixB, matrixG, matrixR))
     final = matrixColor[bordersize:matrixColor.shape[1]-bordersize,bordersize:matrixColor.shape[0]-bordersize].copy()
     # plt.title("color")
     # plt.axis("off")
@@ -141,8 +179,7 @@ def testColor(title, matrixColor):
     cv2.imwrite(title + 'ColorFloydSteinberg.png', cv2.bitwise_not(final))
 
 
-
-def testGray(title, matrixGray):
+def testGray(title, matrixGray, filtro):
     #goThroughGray()
     #matrixGray = cv2.cvtColor(cv2.imread("baboon.png"), cv2.COLOR_BGR2GRAY)
     #matrixColor = cv2.imread('baboon.png', cv2.IMREAD_COLOR)
@@ -162,7 +199,7 @@ def testGray(title, matrixGray):
     # plt.close("all")
 
    
-    matrixGray = goThroughGray(matrixGray, bordersize)
+    matrixGray = goThroughGray(matrixGray, bordersize, filtro)
 
     #cut border out
     matrixFinal = matrixGray[bordersize:matrixGray.shape[1]-bordersize,bordersize:matrixGray.shape[0]-bordersize].copy()
@@ -184,17 +221,19 @@ if __name__ == '__main__':
     #Color images
     data, names = getImages("colorful", "/home/marcelomartins/Documentos/8sem/imagens/proj1/images")
     for i in range(len(data)):
-        testColor(names[i], data[i])
+        testColor(names[i], data[i], floyd)
 
-    # #Gray images
-    # data, names = getImages("gray", "/home/marcelomartins/Documentos/8sem/imagens/proj1/images")
-    # for i in range(len(data)):
-    #     testGray(names[i], data[i])
+    #Gray images
+    data, names = getImages("gray", "/home/marcelomartins/Documentos/8sem/imagens/proj1/images")
+    for i in range(len(data)):
+        testGray(names[i], data[i], floyd)
     
 
-    # img = cv2.cvtColor(cv2.imread("images/baboon.png"), cv2.COLOR_BGR2RGB)
-    # testColor("teste", img)
-    # plt.title("color")
+    #print(jarvis.shape)
+
+    # img = cv2.cvtColor(cv2.imread("images/baboon.png"), cv2.IMREAD_COLOR)
+    # testColor("teste", img, floyd)
+
     # plt.axis("off")
     # plt.imshow(img)
     # plt.waitforbuttonpress(0)
