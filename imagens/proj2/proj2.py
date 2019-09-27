b2430226d7c7dae012f49cba0d856addd21f28e9
marcelo-martins <marcelobmartins219@hotmail.com>
2019-09-27 20:05:45 -0300

    no histogram flag created

--- imagens/proj2/proj2.py
+++ imagens/proj2/proj2.py
@@ -7,26 +7,33 @@ import warnings
 import os
 import glob
 import sys
-#warnings.simplefilter("ignore")
+from argparse import RawTextHelpFormatter
+
+warnings.simplefilter("ignore")
 
 def get_parser():
-    parser = argparse.ArgumentParser(description='Global and local filters testing')
-    parser.add_argument('--folder', default = '/home/marcelomartins/Documentos/8sem/imagens/proj2/imagens',
+    parser = argparse.ArgumentParser(description='Global and local filters testing', formatter_class=RawTextHelpFormatter)
+    parser.add_argument('--folder', default = 'imagens/',
                         help='Folder where the image(s) are')#required = False é um argumento
-    parser.add_argument('--option', type=int, choices = range(0,9), 
-                        help = 'Choose filter you want to run')
-
-    parser.add_argument('--mask', type=int, choices = range(0,100), default = 3,
+    parser.add_argument('--option', type=int, choices = range(1,10),
+                        help = 'Choose filter you want to run\n1 : global\n2 : bernsen\n3 : niblack\n4 : sauvola\n'
+                            + '5 : sabale\n6 : contrast\n7 : mean\n8 : median\n')
+    parser.add_argument('--mask', type=int, choices = range(0,100), default = 3, metavar = 'range 1-100',
                         help = 'Choose size of the mask')
+    parser.add_argument('--image', default = "baboon", help = 'Choose name of the PGM image you want to run')
+    parser.add_argument('--hist', default = 'y', choices = ["y", "n", "Y", "N"], help = 'Choose if you want to generate histogram y/n')
 
     arguments = parser.parse_args()
     return arguments
 
-def plotAndSave(method, black, size, img, final, mask=3):
+def plotAndSave(method, black, size, img, final, mask=3, hist = "y"):
     black = format((black*100/size), ".2f")
-    plt.hist(img.ravel(),256,[0,256])
-    #cv2.imwrite(f"{method}_{mask}x{mask}.png", final) #change format to png
-    plt.savefig(f"{method}_histogram_{mask}x{mask}_{black}%black.png")
+    cv2.imwrite(f"{method}_{mask}x{mask}.png", final) #change format to png
+    if(hist.lower() == 'y'):
+        plt.hist(img.ravel(),256,[0,256])
+        plt.savefig(f"{method}_histogram_{mask}x{mask}_{black}%black.png")
+    else:
+        print(f"Percentage of black pixels = {black}%\n")
 
 def initializeLocalMethod(mask, img):
     borda = math.floor(mask/2)
@@ -38,8 +45,7 @@ def initializeLocalMethod(mask, img):
 
     return borda, black, size, final, height, width
 
-
-def globalMethod(img, nome="nome"):# 0 is height, 1 is weight but 0 is y and weight is x
+def globalMethod(img, nome="nome", hist="y"):# 0 is height, 1 is weight but 0 is y and weight is x
     print("You chose global\n")
 
     final = img.copy() 
@@ -54,10 +60,15 @@ def globalMethod(img, nome="nome"):# 0 is height, 1 is weight but 0 is y and wei
                 final[y,x] = 0
                 black+=1
 
-    plotAndSave(f"{nome}_global", black, size, img, final)
-    
-    
-def bernsen(img, mask=3, nome="nome"): #higher than T is black, otherwise is white
+    black = format((black*100/size), ".2f")
+    cv2.imwrite(f"{name}_global.png", final)
+    if(hist.lower() == 'y'):
+        plt.hist(img.ravel(),256,[0,256])
+        plt.savefig(f"global_histogram_{black}%black.png")
+    else:
+        print(f"Percentage of black pixels = {black}%\n")
+       
+def bernsen(img, mask=3, nome="nome", hist="y"): #higher than T is black, otherwise is white
     print("You chose bernsen\n")
 
     borda, black, size, final, height, width = initializeLocalMethod(mask, img)
@@ -74,9 +85,9 @@ def bernsen(img, mask=3, nome="nome"): #higher than T is black, otherwise is whi
                 final[y,x] = 0
                 black+=1
     
-    plotAndSave(f"{nome}_bernsen", black, size, img, final, mask)
+    plotAndSave(f"{nome}_bernsen", black, size, img, final, mask, hist)
 
-def niblack(img, mask=3, nome="nome"):
+def niblack(img, mask=3, nome="nome", hist="y"):
     print("You chose niblack\n")
 
     k=0.2
@@ -94,9 +105,9 @@ def niblack(img, mask=3, nome="nome"):
                 final[y,x] = 0
                 black+=1
             
-    plotAndSave(f"{nome}_niblack_k{k}", black, size, img, final, mask)
+    plotAndSave(f"{nome}_niblack_k{k}", black, size, img, final, mask, hist)
 
-def sauvola(img, mask=3, nome="nome"):
+def sauvola(img, mask=3, nome="nome", hist="y"):
     print("You chose sauvola\n")
 
     borda, black, size, final, height, width = initializeLocalMethod(mask, img)
@@ -115,9 +126,9 @@ def sauvola(img, mask=3, nome="nome"):
                 final[y, x] = 0
                 black+=1
             
-    plotAndSave(f"{nome}_sauvola", black, size, img, final, mask)
+    plotAndSave(f"{nome}_sauvola", black, size, img, final, mask, hist)
 
-def sabale(img, mask=3, nome="nome"):
+def sabale(img, mask=3, nome="nome", hist="y"):
     print("You chose sabale\n")
 
     borda, black, size, final, height, width = initializeLocalMethod(mask, img)
@@ -137,9 +148,9 @@ def sabale(img, mask=3, nome="nome"):
                 final[y,x] = 0
                 black+=1
             
-    plotAndSave(f"{nome}_sabale", black, size, img, final, mask)
+    plotAndSave(f"{nome}_sabale", black, size, img, final, mask, hist)
 
-def contrast(img, mask=3, nome="nome"):
+def contrast(img, mask=3, nome="nome", hist="y"):
     print("You chose constrast\n")
 
     borda, black, size, final, height, width = initializeLocalMethod(mask, img)
@@ -157,9 +168,9 @@ def contrast(img, mask=3, nome="nome"):
                 final[y,x] = 0
                 black +=1
             
-    plotAndSave(f"{nome}_contrast", black, size, img, final, mask)
+    plotAndSave(f"{nome}_contrast", black, size, img, final, mask, hist)
 
-def mean(img, mask=3, nome="nome"):
+def mean(img, mask=3, nome="nome", hist="y"):
     print("You chose mean\n")
 
     borda, black, size, final, height, width = initializeLocalMethod(mask, img)
@@ -174,9 +185,9 @@ def mean(img, mask=3, nome="nome"):
                 final[y,x] = 0
                 black+=1
             
-    plotAndSave(f"{nome}_mean", black, size, img, final, mask)
+    plotAndSave(f"{nome}_mean", black, size, img, final, mask, hist)
 
-def mediana(img, mask=3, nome="nome"):
+def mediana(img, mask=3, nome="nome", hist="y"):
     print("You chose mediana\n")
 
     borda, black, size, final, height, width = initializeLocalMethod(mask, img)
@@ -191,7 +202,7 @@ def mediana(img, mask=3, nome="nome"):
                 final[y,x] = 0
                 black+=1
             
-    plotAndSave(f"{nome}_mediana", black, size, img, final, mask)
+    plotAndSave(f"{nome}_mediana", black, size, img, final, mask, hist)
 
 def getImages(image_dir):
     data_path = os.path.join(image_dir, '*g')
@@ -207,58 +218,37 @@ def getImages(image_dir):
 if __name__ == '__main__':
     
     arguments = get_parser()
-    img = cv2.imread('imagens/sonnet.pgm', cv2.IMREAD_UNCHANGED)
-
     opt = int(arguments.option)
     mask = int(arguments.mask)
     folder = arguments.folder
-    data, names = getImages(folder)
+    name = arguments.image
+    img = cv2.imread(folder + name + '.pgm', cv2.IMREAD_UNCHANGED)
+    hist = arguments.hist
 
-    count = 0
-    opt = 1
-    mask = 3
-    # for i in range(len(data)):
-    #     img = data[i]
-    #     nome = names[i]
-    nome = "sonnet"
-    while(True):
-        if(opt==1):
-            globalMethod(img, nome)
-        elif(opt==2):
-            bernsen(img, mask, nome)
-        elif(opt==3):
-            niblack(img, mask, nome)
-        elif(opt==4):
-            sauvola(img, mask, nome)
-        elif(opt==5):
-            sabale(img, mask, nome)
-        elif(opt==6):
-            contrast(img, mask, nome)
-        elif(opt==7):
-            mean(img, mask, nome)
-        elif(opt==8):
-            mediana(img, mask, nome)
-        else:
-            mask+=2
-            opt = 1
-            if(mask==9):
-                #mask=3
-                break
-        opt += 1
-
-    # if(opt==1):
-    #     globalMethod(img)
-    # elif(opt==2):
-    #     bernsen(img, mask)
-    # elif(opt==3):
-    #     niblack(img, mask)
-    # elif(opt==4):
-    #     sauvola(img, mask)
-    # elif(opt==5):
-    #     sabale(img, mask)
-    # elif(opt==6):
-    #     contrast(img, mask)
-    # elif(opt==7):
-    #     mean(img, mask)
-    # elif(opt==8):
-    #     mediana(img, mask)
+    if(opt==1):
+        globalMethod(img, name, hist)
+    elif(opt==2):
+        bernsen(img, mask, name, hist)
+    elif(opt==3):
+        niblack(img, mask, name, hist)
+    elif(opt==4):
+        sauvola(img, mask, name, hist)
+    elif(opt==5):
+        sabale(img, mask, name, hist)
+    elif(opt==6):
+        contrast(img, mask, name, hist)
+    elif(opt==7):
+        mean(img, mask, name, hist)
+    elif(opt==8):
+        mediana(img, mask, name, hist)
+    elif(opt==9):
+        print(f"\nExecuting everything for mask = {mask}\n------------------------------------------------------------------")
+        globalMethod(img, name, hist)
+        bernsen(img, mask, name, hist)
+        niblack(img, mask, name, hist)
+        sauvola(img, mask, name, hist)
+        sabale(img, mask, name, hist)
+        contrast(img, mask, name, hist)
+        mean(img, mask, name, hist)
+        mediana(img, mask, name, hist)
+        
