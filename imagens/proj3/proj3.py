3d79417e849b3a5a583ac3cfdca21cd1d640f4eb
marcelo-martins <marcelobmartins219@hotmail.com>
2019-10-08 21:21:57 -0300

    objetos2 image still not working

--- imagens/proj3/proj3.py
+++ imagens/proj3/proj3.py
@@ -6,7 +6,7 @@ import math
 import warnings
 from argparse import RawTextHelpFormatter
 
-#warnings.simplefilter("ignore")
+warnings.simplefilter("ignore")
 
 def get_parser():
     parser = argparse.ArgumentParser(description='Global and local filters testing', formatter_class=RawTextHelpFormatter)
@@ -23,39 +23,41 @@ def plotAndSave(img, nome = "NaN", func = "NaF"):
     cv2.imwrite(f"{func}{nome}.png", img)
 
 def binaryImage(img, nome="NaN"):
+    
     final = img.copy() 
-
     final = cv2.cvtColor(final, cv2.COLOR_BGR2GRAY)
-  
     (thresh, final) = cv2.threshold(final, 254, 255, cv2.THRESH_BINARY)
     plotAndSave(final, nome, "binary_")
 
     return final
 
-def grayscales(img, nome="NaN"):
+def grayscales(img, nome="NaN", generate="y"):
+    
     final = img.copy() 
-
     final = cv2.cvtColor(final, cv2.COLOR_BGR2GRAY)
-    plotAndSave(final, nome, "grayscale_")
+    
+    if generate=="y":
+        plotAndSave(final, nome, "grayscale_")
 
     return final
 
-def edge_detection(img, nome="NaN"):
+def edge_detection(img, nome="NaN", generate="y"):
     final = img.copy()
     v = np.median(final)                                         #sepa eh melhor tirar isso
     lower = int(max(0, (1.0 - 0.33) * v))
     upper = int(min(255, (1.0 + 0.33) * v))
-    final = grayscales(final, nome)
+    final = grayscales(final, nome, "n")
     edges = cv2.Canny(final, lower, upper)
 
     (thresh, edges) = cv2.threshold(edges, 254, 255, cv2.THRESH_BINARY)
     #edges = cv2.Canny(final,100,200)
-    plotAndSave(cv2.bitwise_not(edges), nome, "edges_")
+    if generate=="y":
+        plotAndSave(cv2.bitwise_not(edges), nome, "edges_")
 
     return edges
 
 def contour_properties(img, nome="NaN", printHist = "n"):
-    edges = edge_detection(img, nome)
+    edges = edge_detection(img, nome, "n")
     contours, h = cv2.findContours(edges, 0, 3) #0,3
     
     [peq, med, gra] = [0, 0, 0]
@@ -81,7 +83,6 @@ def contour_properties(img, nome="NaN", printHist = "n"):
         (x, y), (MA, ma), angle = cv2.fitEllipse(cont)
         a = ma/2
         b = MA/2
-
         eccentricity = math.sqrt(pow(a, 2)-pow(b, 2))
         eccentricity = round(eccentricity/a, 2)
 
@@ -97,9 +98,9 @@ def contour_properties(img, nome="NaN", printHist = "n"):
         edges = printIn(edges, nome, cx, cy, i)
         i+=1
     
-    plotAndSave(cv2.bitwise_not(edges), nome, "_numbers")
-
-    if(printHist == "y"):
+    if(printHist=="n"):
+        plotAndSave(cv2.bitwise_not(edges), nome, "numbers_")
+    else:
         print("número de regiões pequenas: " + str(peq) + "\nnúmero de regiões médias: " + str(med) + "\nnúmero de regiões grandes: " + str(gra))
 
         objects = ('Pequenas', 'Médias', 'Grandes')
@@ -111,22 +112,18 @@ def contour_properties(img, nome="NaN", printHist = "n"):
         plt.ylabel('Número de objetos')
         plt.xlabel('Área')
         plt.title('Histograma de áreas de objetos')
-
         plt.savefig(f"hist_areas_{nome}.png")
-        hist = plt.show()
         
-    
 def hist(img, nome="NaN"):
     contour_properties(img, nome, "y")
 
 def printIn(img, nome, cx, cy, i):
     font = cv2.FONT_HERSHEY_SIMPLEX
     writeIn = (cx,cy)
-    fontScale = 0.5
+    fontSize = 0.5
     fontColor = (255)
-    lineType = 2
 
-    cv2.putText(img, str(i), writeIn, font, fontScale, fontColor, lineType)
+    cv2.putText(img, str(i), writeIn, font, fontSize, fontColor, 2)
     
     return img
 
