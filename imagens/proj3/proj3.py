3325e9000b78a14659bd3c3a24bbd4c380fd8e7b
marcelo-martins <marcelobmartins219@hotmail.com>
2019-10-09 20:19:38 -0300

    code basically done

--- imagens/proj3/proj3.py
+++ imagens/proj3/proj3.py
@@ -22,52 +22,45 @@ def get_parser():
 def plotAndSave(img, nome = "NaN", func = "NaF"):
     cv2.imwrite(f"{func}{nome}.png", img)
 
-def binaryImage(img, nome="NaN"):
+def binaryImage(img, nome="NaN", generate="y"):
     
-    final = img.copy() 
-    final = cv2.cvtColor(final, cv2.COLOR_BGR2GRAY)
+    final = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     (thresh, final) = cv2.threshold(final, 254, 255, cv2.THRESH_BINARY)
-    plotAndSave(final, nome, "binary_")
+    if generate == "y":
+        plotAndSave(final, nome, "binary_")
 
     return final
 
 def grayscales(img, nome="NaN", generate="y"):
     
-    final = img.copy() 
-    final = cv2.cvtColor(final, cv2.COLOR_BGR2GRAY)
-    
+    final = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     if generate=="y":
         plotAndSave(final, nome, "grayscale_")
 
     return final
 
 def edge_detection(img, nome="NaN", generate="y"):
-    final = img.copy()
-    v = np.median(final)                                         #sepa eh melhor tirar isso
-    lower = int(max(0, (1.0 - 0.33) * v))
-    upper = int(min(255, (1.0 + 0.33) * v))
-    final = grayscales(final, nome, "n")
-    edges = cv2.Canny(final, lower, upper)
-
-    (thresh, edges) = cv2.threshold(edges, 254, 255, cv2.THRESH_BINARY)
-    #edges = cv2.Canny(final,100,200)
+    
+    final = binaryImage(img, nome, "n").astype('uint8')
+    contours, _ = cv2.findContours(final, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    edges = cv2.drawContours(np.full(img.shape, 255), contours[1:], -1, (0, 0, 255))
     if generate=="y":
-        plotAndSave(cv2.bitwise_not(edges), nome, "edges_")
+        plotAndSave(edges, nome, "edges_")
 
-    return edges
+    return edges, contours
 
 def contour_properties(img, nome="NaN", printHist = "n"):
-    edges = edge_detection(img, nome, "n")
-    contours, h = cv2.findContours(edges, 0, 3) #0,3
+    _, contours = edge_detection(img, nome, "n")
+    edges = binaryImage(img.copy(), nome, "n")
     
     [peq, med, gra] = [0, 0, 0]
     if printHist=="n":
-        print("Número de regiões: " + str(len(contours)) + "\n")
+        print("Número de regiões: " + str(len(contours)-1) + "\n")
     
-    i=0
-    for cont in contours:
+    i=0  
+    for cont in (reversed(contours)):
+        
         area = cv2.contourArea(cont)
-
         if(area<1500):
             peq += 1
         elif(area>=1500 and area<3000):
@@ -76,9 +69,10 @@ def contour_properties(img, nome="NaN", printHist = "n"):
             gra += 1
 
         perimeter = cv2.arcLength(cont, True)
+        
         hull = cv2.convexHull(cont)
         hull_area = cv2.contourArea(hull)
-        solidity = float(area)/hull_area
+        solidity = area/hull_area
 
         (x, y), (MA, ma), angle = cv2.fitEllipse(cont)
         a = ma/2
@@ -86,29 +80,29 @@ def contour_properties(img, nome="NaN", printHist = "n"):
         eccentricity = math.sqrt(pow(a, 2)-pow(b, 2))
         eccentricity = round(eccentricity/a, 2)
 
-        if printHist == "n":
-            print("Região " + str(i) + ": Área: " + str(format(math.ceil(area), "4.0f")) 
-                + " Perímetro:  " + str(format(perimeter, "4.6f")) 
-                + " Excentricidade: " + str(format(eccentricity, "4.6f")) 
-                + " Solidez: " + str(format(solidity, "3.6f")))
-        
         M = cv2.moments(cont)
+        if printHist=="n":
+            print("Região {0:{1}d}: Área: {2:4.0f} Perímetro: {3:10.6f} Excentricidade: {4:10.6f} Solidez: {5:10.6f}"
+                .format(i, 8, area, perimeter, eccentricity, solidity))
+            
         cx = int(M['m10']/M['m00'])
         cy = int(M['m01']/M['m00'])
         edges = printIn(edges, nome, cx, cy, i)
         i+=1
+        if(i==len(contours)-1):
+            break
     
     if(printHist=="n"):
-        plotAndSave(cv2.bitwise_not(edges), nome, "numbers_")
+        plotAndSave(edges, nome, "numbers_")
     else:
         print("número de regiões pequenas: " + str(peq) + "\nnúmero de regiões médias: " + str(med) + "\nnúmero de regiões grandes: " + str(gra))
 
-        objects = ('Pequenas', 'Médias', 'Grandes')
-        y_pos = np.arange(len(objects))
-        performance = [peq, med, gra]
+        x_titles = ('Pequenas', 'Médias', 'Grandes')
+        y_pos = np.arange(len(x_titles))
+        sizes = [peq, med, gra]
 
-        plt.bar(y_pos, performance, align='center', alpha=1)
-        plt.xticks(y_pos, objects)
+        plt.bar(y_pos, sizes, align='center', alpha=1)
+        plt.xticks(y_pos, x_titles)
         plt.ylabel('Número de objetos')
         plt.xlabel('Área')
         plt.title('Histograma de áreas de objetos')
@@ -119,11 +113,11 @@ def hist(img, nome="NaN"):
 
 def printIn(img, nome, cx, cy, i):
     font = cv2.FONT_HERSHEY_SIMPLEX
-    writeIn = (cx,cy)
-    fontSize = 0.5
-    fontColor = (255)
+    writeIn = (cx-6,cy+3)
+    fontSize = 0.3
+    fontColor = (255,255,255)
 
-    cv2.putText(img, str(i), writeIn, font, fontSize, fontColor, 2)
+    cv2.putText(img, str(i), writeIn, font, fontSize, fontColor, 1)
     
     return img
 
